d26e186fe34db135bd42840dfc0d6496dfaf7f15
Enrique de la Torre <indisoluble_dev@me.com>
2015-01-31 19:19:46 +0000

    New controller: Questions TableViewController

---
+++ Answers/Answers/Controllers/Questions/IAWControllerQuestionsTVC.h
@@ -0,0 +1,15 @@
+//
+//  IAWControllerQuestionsTVC.h
+//  Answers
+//
+//  Created by Enrique de la Torre (dev) on 31/01/2015.
+//  Copyright (c) 2015 Enrique de la Torre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface IAWControllerQuestionsTVC : UITableViewController
+
+@end
---
+++ Answers/Answers/Controllers/Questions/IAWControllerQuestionsTVC.m
@@ -0,0 +1,67 @@
+//
+//  IAWControllerQuestionsTVC.m
+//  Answers
+//
+//  Created by Enrique de la Torre (dev) on 31/01/2015.
+//  Copyright (c) 2015 Enrique de la Torre. All rights reserved.
+//
+
+#import "IAWControllerQuestionsTVC.h"
+
+
+
+@interface IAWControllerQuestionsTVC ()
+
+@end
+
+
+
+@implementation IAWControllerQuestionsTVC
+
+#pragma mark - Memory management
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - View lifecycle
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+
+#pragma mark - Navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+
+#pragma mark - UITableViewDataSource methods
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+@end
